d84d44d44ee4fb5aa96bbb246dd5250ebb84d579
MRootxx <dr.rootvb@gmail.com>
2026-06-06 17:10:35 +0300

    Fix CSP: allow Google Fonts and inline scripts for Facebook Pixel

--- app/Http/Middleware/SecurityHeaders.php
+++ app/Http/Middleware/SecurityHeaders.php
@@ -29,12 +29,12 @@ class SecurityHeaders
         // Permissions policy
         $response->headers->set('Permissions-Policy', 'geolocation=(), microphone=(), camera=()');
 
-        // Content Security Policy (strict)
+        // Content Security Policy
         $csp = "default-src 'self'; " .
-               "script-src 'self' https://connect.facebook.net; " .
-               "style-src 'self' 'unsafe-inline'; " .
+               "script-src 'self' 'unsafe-inline' https://connect.facebook.net; " .
+               "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com; " .
                "img-src 'self' data: blob: https://www.facebook.com; " .
-               "font-src 'self'; " .
+               "font-src 'self' https://fonts.gstatic.com; " .
                "connect-src 'self' https://api.otpiq.com https://graph.facebook.com; " .
                "frame-ancestors 'none'; " .
                "base-uri 'self'; " .
